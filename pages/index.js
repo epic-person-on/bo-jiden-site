9ce0ed52aca0c4a07094a39f258e0fddace99787
Me <123142178+epic-person-on@users.noreply.github.com>
2024-07-11 19:54:17 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const products = [
-  { url: 'https://example.com', name: 'Bo Jiden Mask', image: 'mask.jpg' },
-  { url: 'https://example.com', name: 'Bo Jiden Wig', image: 'wig.jpg' },
-  { url: 'https://example.com', name: 'Invincible Man Light sword', image: 'sword.jpg' },
+  { url: 'https://creations.mtdv.me/Bojidenmask', name: 'Bo Jiden Mask', image: 'https://m.media-amazon.com/images/I/51wi7VqKroL._AC_SX679_.jpg' },
+  { url: 'https://creations.mtdv.me/Bojidenmask', name: 'Bo Jiden Wig', image: 'https://i.etsystatic.com/18173219/r/il/1865a2/5388230307/il_570xN.5388230307_7pgg.jpg' },
+  { url: 'https://creations.mtdv.me/Bojidenmask', name: 'Invincible Man Light sword', image: '' },
   // Add more products as needed
 ];
 
